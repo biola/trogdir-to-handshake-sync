3c5c9dee7223fc19cd24b5efcb2d3eeed1a4483f
O. Bucao <orlando.bucao@biola.edu>
2016-07-22 13:07:42 -0700

    Rename Base.create_accessor and Base.create_reader

--- lib/handshake/models/base.rb
+++ lib/handshake/models/base.rb
@@ -7,18 +7,18 @@ class Handshake::Models::Base
 
   def self.define_accessors(attributes)
     attributes.map(&:to_s).each do |name|
-      create_accessor(name)
-      create_reader(name)
+      create_setter(name)
+      create_getter(name)
     end
   end
 
-  def self.create_accessor(name)
+  def self.create_setter(name)
     create_instance_method("#{name}=".to_sym) do |val|
       instance_variable_set("@" + name, val)
     end
   end
 
-  def self.create_reader(name)
+  def self.create_getter(name)
     create_instance_method(name.to_sym) do
       instance_variable_get("@" + name)
     end
